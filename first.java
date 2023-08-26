df68b72e5c201bddd4c2163054bc194e6ac3d54c
Raushan <raushanmandallkr@gmail.com>
2023-08-26 02:10:51 -0700

    To take a input whole Next line[String-type]

--- first.java
+++ first.java
@@ -1,15 +1,15 @@
-import java.util.*; // to use [sc] object from the [Scanner] to take inputs
+import java.util.Scanner; // to use [sc] object from the [Scanner] to take inputs
 
 public class first {
     public static void main(String[] args) {
         System.out.println("Enter your values -> which is String type: ");
-        Scanner sc = new Scanner(System.in); // sc is the (object)
+        Scanner scannerObject = new Scanner(System.in); // scannerObject is the (object) of [Scanner -> Class]
         // String name = sc.next(); // this will scan and print the next 1
         // token(string,or words) only
         // taking input into [name] and perform by [next()] function ->
         // passed through by [sc] object, which carry the input objects
 
-        String name = sc.nextLine(); // this will scan and print the next 1 LINE (string, or words) only
+        String name = scannerObject.nextLine(); // this will scan and print the next 1 LINE (string, or words) only
         // String name = sc.nextInt(); // this will scan and print the next 1 LINE
         // (Integer type values) only
         // String name = sc.nextFloat(); // this will scan and print the next 1 LINE
