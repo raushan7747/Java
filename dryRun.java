75e4ce665f42b141b404c1203139da909b2bea6c
Raushan Kumar <137276147+raushan7747@users.noreply.github.com>
2023-10-29 18:58:29 -0700

    Interpretation of Dry run with example

--- dryRun.java
+++ dryRun.java
@@ -1,8 +1,6 @@
 /* dryRun means the : Whenever we don't want to repeat my task, we put our code to the Function and
- * whenever we needed just call them to repeat the same tasks, there may be need to calculate even
- * most complex formulas, and we don't wanna to be repeat that complex formulas when we needed to
- * do even more than a time. So, the another way is that, we just call our function to do that 
- * type of work as much better
+ * whenever we needed just call them to repeat the same tasks, there may be need to calculate even most complex formulas, and we don't wanna to be repeat that complex formulas when we needed to do even more than a time. So, the another way is that, we just call our function to do that type of work as much better.
+
  * one more thing, that's why we should to do dry run is that, particular code block can be checked easily, and this practice enhance the code re-usibility and maintanence while updating the code or when we add new things, or also it is easily understandable and more visible to the naive user also.
 we can test that particular code to find any problems if they exist, that's why we should practice to keep the almost code blocks in Functions for the dry run maintanence also.
  */
